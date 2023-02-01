d624687db8e7fb4993855034c948f0161b47e9dc
Shivayshiva <prashantmaddheshiya@gmail.com>
2023-02-01 22:23:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from './Character'
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState("")
+  const [passwordLength, setPasswordLength] = useState(26)
+  const [includeUpperCase, setIncludeUpperCase] = useState(false)
+  const [includeLowerCase, setIncludeLowerCase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+  const handleGeneratePassword = () => {
+    if (!includeUpperCase && !includeLowerCase && !includeNumbers && !includeSymbols) {
+      // notify("To generate password you must select atleast one checkbox", true)
+    }
+    else {
+      let characterList = ""
+      if (includeNumbers) {
+        characterList = characterList + numbers
+      }
+      if (includeUpperCase) {
+        characterList = characterList + upperCaseLetters
+      }
+      if (includeLowerCase) {
+        characterList = characterList + lowerCaseLetters
+      }
+      if (includeSymbols) {
+        characterList = characterList + specialCharacters
+      }
+      setPassword(createPassword(characterList))
+      // notify("Password is generated successfully", false)
+    }
+
+
+  }
+  const createPassword = (characterList) => {
+    let password = ""
+    const characterListLength = characterList.length
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength)
+      password = password + characterList.charAt(characterIndex)
+    }
+    return password
+  }
+
+  // const copyToClipboard = (password) => {
+  //   navigator.clipboard.writeText(password)
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator__header">
+           Password Generator
+          </h2>
+          <div className="generator__password">
+            {password}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-strength">Password length</label>
+            <input className="pw" defaultValue={passwordLength} onChange={(e) => setPasswordLength(e.target.value)} type="number" id="password-stregth" name="password-strength" max="26" min="8" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Add Uppercase Letters</label>
+            <input checked={includeUpperCase} onChange={(e) => setIncludeUpperCase(e.target.checked)} type="checkbox" id="uppercase-letters" name="uppercase-letters" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Add Lowercase Letters</label>
+            <input checked={includeLowerCase} onChange={(e) => setIncludeLowerCase(e.target.checked)} type="checkbox" id="lowercase-letters" name="lowercase-letters" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)} type="checkbox" id="include-numbers" name="include-numbers" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.checked)} type="checkbox" id="include-symbols" name="include-symbols" />
+          </div>
+          <button onClick={handleGeneratePassword} className="generator__btn">
+            Generate Password
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
